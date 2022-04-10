1dccdebdd60830a69407d5ed0c39ddd775abb571
suatt-prog <72493647+suatt-prog@users.noreply.github.com>
2022-04-10 18:42:25 +0300

    Update C#FileRead.py

--- C#FileRead.py
+++ C#FileRead.py
@@ -1,5 +1,6 @@
 import os
 with open("oku.txt","a") as dosya:
-    dosya.write("suat")
+    a=input("Enter the text input you want to give to the C# file")
+    dosya.write(a)
     #The address of your c# exe file should be given to os.startfile
     os.startfile("C://Users//90553//source//repos//fileread//fileread//bin//Debug//netcoreapp3.1//fileread.exe")
